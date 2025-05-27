8755199931812e285861102d48beb6a94dd2a8ce
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-05-26 23:36:04 -0500

    Add cli support

--- python/libcuopt/libcuopt/tests/test_cli.sh
+++ python/libcuopt/libcuopt/tests/test_cli.sh
@@ -21,13 +21,14 @@ set -euo pipefail
 
 
 # Add a test for the help command
-#cuopt_cli --help | grep -q "Usage: cuopt_cli" || (echo "Expected usage information not found" && exit 1)
+cuopt_cli --help | grep -q "Usage: cuopt_cli" || (echo "Expected usage information not found" && exit 1)
 
 # Add a test with a simple linear programming problem
 
 # Run solver and check for optimal status - fail if not found
-#cuopt_cli ${RAPIDS_DATASET_ROOT_DIR}/linear_programming/good-mps-1.mps | grep -q "Status: " || (echo "Expected solution not found" && exit 1)
+
+cuopt_cli ${RAPIDS_DATASET_ROOT_DIR}/linear_programming/good-mps-1.mps | grep -q "Status: " || (echo "Expected solution not found" && exit 1)
 
 # Add a for mixed integer programming test with options
 
-#cuopt_cli ${RAPIDS_DATASET_ROOT_DIR}/mip/sample.mps --mip-absolute-gap 0.01 --time-limit 10 | grep -q "Solution objective" || (echo "Expected solution objective not found" && exit 1)
+cuopt_cli ${RAPIDS_DATASET_ROOT_DIR}/mip/sample.mps --mip-absolute-gap 0.01 --time-limit 10 | grep -q "Solution objective" || (echo "Expected solution objective not found" && exit 1)
