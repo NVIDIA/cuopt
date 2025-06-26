113144897f7b04a6ffc6726e42ef2b66f8efecb2
Rajesh Gandham <rgandham@nvidia.com>
2025-06-26 07:58:15 -0700

    Make sure to negate the objective offset as well

--- cpp/src/mip/problem/problem_helpers.cuh
+++ cpp/src/mip/problem/problem_helpers.cuh
@@ -138,6 +138,9 @@ static void convert_to_maximization_problem(detail::problem_t<i_t, f_t>& op_prob
   // negating objective coeffs
   op_problem.presolve_data.objective_scaling_factor =
     -op_problem.presolve_data.objective_scaling_factor;
+
+  // Negate objective offset
+  op_problem.presolve_data.objective_offset = -op_problem.presolve_data.objective_offset;
 }
 
 /*
