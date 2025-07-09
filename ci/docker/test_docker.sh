c444d906fb217b3f19d698385eb647d515be1dbc
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-07-08 21:00:38 -0500

    fix cli test and remove exhaustive testing

--- ci/docker/test_docker.sh
+++ ci/docker/test_docker.sh
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -euo pipefail
+
 # Install dependencies
 su cuopt -c "pip install --user pytest"
 
@@ -25,15 +27,13 @@ cd datasets && ./get_test_data.sh --solomon && ./get_test_data.sh --tsp && cd -
 
 # Test CLI
 echo "Testing CLI"
-su cuopt -c "export RAPIDS_DATASET_ROOT_DIR=$(realpath datasets) && bash python/libcuopt/libcuopt/tests/test_cli.sh"
+su cuopt -c "export PATH=$PATH:/home/cuopt/.local/bin && export RAPIDS_DATASET_ROOT_DIR=$(realpath datasets) && bash python/libcuopt/libcuopt/tests/test_cli.sh"
 
 # Test cuopt
 echo "Testing cuopt"
 su cuopt -c "RAPIDS_DATASET_ROOT_DIR=./datasets python -m pytest python/cuopt/cuopt/tests/routing/test_vehicle_routing.py"
 su cuopt -c "RAPIDS_DATASET_ROOT_DIR=./datasets python -m pytest python/cuopt/cuopt/tests/linear_programming/test_lp_solver.py::test_solver"
-su cuopt -c "RAPIDS_DATASET_ROOT_DIR=./datasets python -m pytest python/cuopt/cuopt/tests/"
 
 # Test cuopt server
 echo "Testing cuopt server"
 su cuopt -c "RAPIDS_DATASET_ROOT_DIR=./datasets python -m pytest python/cuopt_server/cuopt_server/tests/test_server.py"
-su cuopt -c "RAPIDS_DATASET_ROOT_DIR=./datasets python -m pytest python/cuopt_server/cuopt_server/tests/"
