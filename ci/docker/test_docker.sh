a15833903fb947f16a73dc48f1a0857fc2d00357
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-07-09 12:19:45 -0500

    Add fix to testing

--- ci/docker/test_docker.sh
+++ ci/docker/test_docker.sh
@@ -18,22 +18,23 @@
 set -euo pipefail
 
 # Install dependencies
-su cuopt -c "pip install --user pytest"
+su cuopt -c "pip install --user pytest pexpect"
 
 # Download test data
 bash datasets/linear_programming/download_pdlp_test_dataset.sh
 bash datasets/mip/download_miplib_test_dataset.sh
-cd datasets && ./get_test_data.sh --solomon && ./get_test_data.sh --tsp && cd -
+ln -sf "$(pwd)" /home/cuopt/cuopt
+
+
 
 # Test CLI
 echo "Testing CLI"
-su cuopt -c "export PATH=$PATH:/home/cuopt/.local/bin && export RAPIDS_DATASET_ROOT_DIR=$(realpath datasets) && bash python/libcuopt/libcuopt/tests/test_cli.sh"
+su - cuopt -c "cd cuopte && export RAPIDS_DATASET_ROOT_DIR=$(realpath datasets) && bash python/libcuopt/libcuopt/tests/test_cli.sh"
 
 # Test cuopt
 echo "Testing cuopt"
-su cuopt -c "RAPIDS_DATASET_ROOT_DIR=./datasets python -m pytest python/cuopt/cuopt/tests/routing/test_vehicle_routing.py"
-su cuopt -c "RAPIDS_DATASET_ROOT_DIR=./datasets python -m pytest python/cuopt/cuopt/tests/linear_programming/test_lp_solver.py::test_solver"
+su - cuopt -c "cd cuopt && RAPIDS_DATASET_ROOT_DIR=./datasets python -m pytest python/cuopt/cuopt/tests/"
 
 # Test cuopt server
 echo "Testing cuopt server"
-su cuopt -c "RAPIDS_DATASET_ROOT_DIR=./datasets python -m pytest python/cuopt_server/cuopt_server/tests/test_server.py"
+su - cuopt -c "cd cuopt && RAPIDS_DATASET_ROOT_DIR=./datasets python -m pytest python/cuopt_server/cuopt_server/tests/test_server.py"
