28096a9258f46d31d894a26aa4a951fdaf063812
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-07-11 10:33:42 -0500

    fix style

--- python/cuopt_server/cuopt_server/tests/utils/utils.py
+++ python/cuopt_server/cuopt_server/tests/utils/utils.py
@@ -14,16 +14,14 @@
 # limitations under the License.
 
 import os
-import signal
 import shutil
+import signal
 import time
 from subprocess import Popen, TimeoutExpired
 from typing import Dict, List, Optional
 
 import pytest
 import requests
-
-from cuopt_server import cuopt_service
 
 RAPIDS_DATASET_ROOT_DIR = os.getenv("RAPIDS_DATASET_ROOT_DIR")
 if RAPIDS_DATASET_ROOT_DIR is None:
