996ad21edaad49852fa35455df6da936ba682bb7
MohdAatifSiddi <mekotecnolagy@gmail.com>
2025-07-12 14:35:47 +0530

    Fix: Create log directory if it doesn't exist in webserver

--- python/cuopt_server/cuopt_server/webserver.py
+++ python/cuopt_server/cuopt_server/webserver.py
@@ -342,8 +342,9 @@ def getsolverlogs(
                 detail=f"Unsupported Accept value {accept}, "
                 f"supported values are {[mime_json, mime_msgpack, mime_zlib]}",
             )
-        # TODO: Create a log dir
+        # Create log directory if it does't exist
         log_dir, _, _ = settings.get_result_dir()
+        os.makedirs(log_dir, exist_ok=True)
         log_fname = "log_" + id
         log_file = os.path.join(log_dir, log_fname)
         logging.info(f"Extracting logs from {log_file}")
