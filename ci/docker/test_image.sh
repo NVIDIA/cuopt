9248e650333a263de1ad5610ad047ff24ac8089d
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-07-15 11:05:30 -0500

    Address review comments

--- ci/docker/test_image.sh
+++ ci/docker/test_image.sh
@@ -20,7 +20,7 @@ set -euo pipefail
 chsh -s /bin/bash cuopt
 
 # Install dependencies
-apt-get install -y file bzip2 gcc
+apt-get install -y --no-install-recommends file bzip2 gcc
 
 # Download test data
 bash datasets/linear_programming/download_pdlp_test_dataset.sh
