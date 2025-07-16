32cf511d95cab659691e4b237cc0f127470e5dcc
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-07-16 16:46:48 -0500

    disable problematic test case

--- cpp/tests/mip/feasibility_jump_tests.cu
+++ cpp/tests/mip/feasibility_jump_tests.cu
@@ -232,9 +232,11 @@ TEST_P(MIPSolveParametricTest, feasibility_jump_obj_test)
 INSTANTIATE_TEST_SUITE_P(
   MIPSolveTest,
   MIPSolveParametricTest,
-  testing::Values(std::make_tuple("50v-10.mps", 7800, 100000),
+  testing::Values(
                   // Bug: https://github.com/NVIDIA/cuopt/issues/214
+                  // std::make_tuple("50v-10.mps", 7800, 100000),
                   // std::make_tuple("fiball.mps", 140, 25000),
+                  // std::make_tuple("rmatr200-p5.mps", 7000, 10000),
                   std::make_tuple("gen-ip054.mps", 7500, 20000),
                   std::make_tuple("sct2.mps", 100, 50000),
                   std::make_tuple("uccase9.mps", 4000000, 50000),
@@ -249,8 +251,6 @@ INSTANTIATE_TEST_SUITE_P(
                   std::make_tuple("gmu-35-50.mps", -2300000, 25000),
                   std::make_tuple("n2seq36q.mps", 158800, 25000),
                   std::make_tuple("seymour1.mps", 440, 50000),
-                  // Bug: https://github.com/NVIDIA/cuopt/issues/214
-                  // std::make_tuple("rmatr200-p5.mps", 7000, 10000),
                   std::make_tuple("cvs16r128-89.mps", -50, 10000)
 // TEMPORARY: occasional cusparse transpose issues on ARM in CI
 #ifndef __aarch64__
