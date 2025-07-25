edaeef75294b206ec9bf73629d6ccda159949c82
Trevor McKay <tmgithub1@gmail.com>
2025-07-25 08:40:29 -0400

    cuopt client include deprecated parameters in ThinClientSolverSettings

--- python/cuopt_self_hosted/cuopt_sh_client/thin_client_solver_settings.py
+++ python/cuopt_self_hosted/cuopt_sh_client/thin_client_solver_settings.py
@@ -177,6 +177,18 @@ class ThinClientSolverSettings:
             "mip_relative_gap",
             "mip_absolute_tolerance",
             "mip_relative_tolerance",
+            # deprecated parameters
+            "absolute_primal",
+            "absolute_dual",
+            "absolute_gap",
+            "relative_primal",
+            "relative_dual",
+            "relative_gap",
+            "primal_infeasible",
+            "dual_infeasible",
+            "integrality_tolerance",
+            "absolute_mip_gap",
+            "relative_mip_gap",
         ]
 
         # Grab everything that is not a tolerance
