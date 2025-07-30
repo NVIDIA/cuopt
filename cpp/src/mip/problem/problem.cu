99d0056f9c28995c0425ec0cf5143504b21b6a31
Ramakrishnap <42624703+rgsl888prabhu@users.noreply.github.com>
2025-07-30 15:01:27 -0500

    Update problem.cu

--- cpp/src/mip/problem/problem.cu
+++ cpp/src/mip/problem/problem.cu
@@ -1208,9 +1208,6 @@ void problem_t<i_t, f_t>::remove_given_variables(problem_t<i_t, f_t>& original_p
 {
   thrust::fill(handle_ptr->get_thrust_policy(), offsets.begin(), offsets.end(), 0);
   cuopt_assert(assignment.size() == n_variables, "Variable size mismatch");
-  std::cout << "variable_map size " << variable_map.size() << std::endl;
-  std::cout << "n_variables " << n_variables << std::endl;
-  printf("variable_map size %zu n_variables %d\n", variable_map.size(), n_variables);
   cuopt_assert(variable_map.size() < n_variables, "Too many variables to fix");
   rmm::device_uvector<f_t> tmp_assignment(assignment, handle_ptr->get_stream());
 
