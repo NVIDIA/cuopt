105e913122d64e71f4d33a00b48a71e22831e471
Ishika Roy <iroy@ipp1-3302.aselab.nvidia.com>
2025-07-30 14:56:39 -0700

    revert cuda-cupy changes and update attributes

--- python/cuopt/cuopt/linear_programming/problem.py
+++ python/cuopt/cuopt/linear_programming/problem.py
@@ -574,10 +574,6 @@ class Problem:
     ----------
     Name : str
         Name of the model.
-    vars : list
-        List of variables in the problem.
-    constrs : list
-        List of constraints in the problem.
     ObjSense : sense
         Objective sense (MINIMIZE or MAXIMIZE).
     Obj : object
@@ -588,22 +584,6 @@ class Problem:
         Status of the problem after solving.
     IsMIP : bool
         Indicates if the problem is a Mixed Integer Program.
-    rhs : array-like
-        Right-hand side values for constraints.
-    row_sense : array-like
-        Senses for each constraint row.
-    row_pointers : array-like
-        Row pointers for constraint matrix.
-    column_indicies : array-like
-        Column indices for constraint matrix.
-    values : array-like
-        Values for constraint matrix.
-    lower_bound : array-like
-        Lower bounds for variables.
-    upper_bound : array-like
-        Upper bounds for variables.
-    var_type : array-like
-        Types of variables (continuous, integer, etc.).
     SolveTime : float
         Time taken to solve the problem.
     SolutionStats : dict
