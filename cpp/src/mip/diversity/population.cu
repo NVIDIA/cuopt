3318970e3f1a1286e97da217f884c029a67ee5bc
akifcorduk <akifcorduk@gmail.com>
2025-08-27 09:04:09 -0700

    increase diversity even more

--- cpp/src/mip/diversity/population.cu
+++ cpp/src/mip/diversity/population.cu
@@ -59,15 +59,15 @@ template <typename i_t>
 i_t get_max_var_threshold(i_t n_vars)
 {
   if (n_vars < 50) {
-    return std::max(1, n_vars - 1);
+    return std::max(1, n_vars - 2);
   } else if (n_vars < 80) {
-    return n_vars - 3;
+    return n_vars - 6;
   } else if (n_vars < 200) {
-    return n_vars - 5;
-  } else if (n_vars < 1000) {
     return n_vars - 15;
+  } else if (n_vars < 1000) {
+    return n_vars - 40;
   }
-  return n_vars - 30;
+  return n_vars - 60;
 }
 
 template <typename i_t, typename f_t>
