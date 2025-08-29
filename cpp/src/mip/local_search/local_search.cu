8225802e3e9a9937958681c78f537b103f72361b
akifcorduk <akifcorduk@gmail.com>
2025-08-29 01:29:49 -0700

    enable recombiation

--- cpp/src/mip/local_search/local_search.cu
+++ cpp/src/mip/local_search/local_search.cu
@@ -431,11 +431,11 @@ bool local_search_t<i_t, f_t>::run_fp(solution_t<i_t, f_t>& solution,
           solution_t<i_t, f_t> solution_copy(solution);
           solution_copy.problem_ptr = old_problem_ptr;
           solution_copy.resize_to_problem();
-          // if (population_ptr->current_size() > 0) {
-          //   resize_to_new_problem();
-          //   population_ptr->run_all_recombiners(solution_copy);
-          //   resize_to_old_problem(old_problem_ptr);
-          // }
+          if (population_ptr->current_size() > 0) {
+            resize_to_new_problem();
+            population_ptr->run_all_recombiners(solution_copy);
+            resize_to_old_problem(old_problem_ptr);
+          }
           population_ptr->add_solution(std::move(solution_copy));
           auto new_sol_vector = population_ptr->get_external_solutions();
           population_ptr->add_solutions_from_vec(std::move(new_sol_vector));
@@ -463,11 +463,11 @@ bool local_search_t<i_t, f_t>::run_fp(solution_t<i_t, f_t>& solution,
             solution_t<i_t, f_t> solution_copy(solution);
             solution_copy.problem_ptr = old_problem_ptr;
             solution_copy.resize_to_problem();
-            // if (population_ptr->current_size() > 0) {
-            //   resize_to_new_problem();
-            //   population_ptr->run_all_recombiners(solution_copy);
-            //   resize_to_old_problem(old_problem_ptr);
-            // }
+            if (population_ptr->current_size() > 0) {
+              resize_to_new_problem();
+              population_ptr->run_all_recombiners(solution_copy);
+              resize_to_old_problem(old_problem_ptr);
+            }
             population_ptr->add_solution(std::move(solution_copy));
             auto new_sol_vector = population_ptr->get_external_solutions();
             population_ptr->add_solutions_from_vec(std::move(new_sol_vector));
