f46c9f202d29f12ff029f5250e17021c6ed19d59
akifcorduk <akifcorduk@gmail.com>
2025-09-08 07:39:12 -0700

    logs enabled

--- cpp/src/dual_simplex/phase2.cpp
+++ cpp/src/dual_simplex/phase2.cpp
@@ -2842,7 +2842,11 @@ dual::status_t dual_phase2(i_t phase,
     } else {
       vstatus[leaving_index] = variable_status_t::NONBASIC_FIXED;
     }
-    basic_list[basic_leaving_index]        = entering_index;
+    basic_list[basic_leaving_index] = entering_index;
+    settings.log.printf("basic_leaving_index %d leaving %d nonbasic.size %lu\n",
+                        basic_leaving_index,
+                        leaving_index,
+                        nonbasic_list.size());
     nonbasic_list[nonbasic_entering_index] = leaving_index;
     nonbasic_mark[entering_index]          = -1;
     nonbasic_mark[leaving_index]           = nonbasic_entering_index;
