cbd0a53b0c098ac4a00378b0bda0fca50b1b4911
Alice Boucher <yboucher@nvidia.com>
2025-09-15 09:23:21 +0000

    rare perturb

--- cpp/src/mip/feasibility_jump/fj_cpu.cu
+++ cpp/src/mip/feasibility_jump/fj_cpu.cu
@@ -880,8 +880,8 @@ std::unique_ptr<fj_cpu_climber_t<i_t, f_t>> fj_t<i_t, f_t>::create_cpu_climber(
     auto rng                 = std::mt19937(cuopt::seed_generator::get_seed());
     fj_cpu->mtm_viol_samples = std::uniform_int_distribution<i_t>(15, 50)(rng);
     fj_cpu->mtm_sat_samples  = std::uniform_int_distribution<i_t>(10, 30)(rng);
-    fj_cpu->nnz_samples      = std::uniform_int_distribution<i_t>(20000, 150000)(rng);
-    fj_cpu->perturb_interval = std::uniform_int_distribution<i_t>(50, 500)(rng);
+    fj_cpu->nnz_samples      = std::uniform_int_distribution<i_t>(2000, 15000)(rng);
+    fj_cpu->perturb_interval = std::uniform_int_distribution<i_t>(500, 5000)(rng);
   }
   fj_cpu->settings.seed = cuopt::seed_generator::get_seed();
   return fj_cpu;  // move
