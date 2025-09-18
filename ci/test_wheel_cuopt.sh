088f2194415f20f69c0dc4a46d62ce2c889c5a33
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-09-18 13:49:29 -0500

    update for testing

--- ci/test_wheel_cuopt.sh
+++ ci/test_wheel_cuopt.sh
@@ -21,6 +21,9 @@ set -euo pipefail
 # so those constraints will affect all future 'pip install' calls
 source rapids-init-pip
 
+git clone --verbose https://github.com/jump-dev/cuOpt.jl /tmp/cuOpt.jl
+cd /tmp/cuOpt.jl
+
 # Download the packages built in the previous step
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
 CUOPT_MPS_PARSER_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="cuopt_mps_parser" rapids-download-wheels-from-github python)
@@ -56,25 +59,25 @@ elif command -v dnf &> /dev/null; then
     dnf -y install file unzip
 fi
 
-./datasets/linear_programming/download_pdlp_test_dataset.sh
-./datasets/mip/download_miplib_test_dataset.sh
-cd ./datasets
-./get_test_data.sh --solomon
-./get_test_data.sh --tsp
-cd -
+#./datasets/linear_programming/download_pdlp_test_dataset.sh
+#./datasets/mip/download_miplib_test_dataset.sh
+#cd ./datasets
+#./get_test_data.sh --solomon
+#./get_test_data.sh --tsp
+#cd -
 
 RAPIDS_DATASET_ROOT_DIR="$(realpath datasets)"
 export RAPIDS_DATASET_ROOT_DIR
 
 # Please enable this once ISSUE https://github.com/NVIDIA/cuopt/issues/94 is fixed
 # Run CLI tests
-timeout 10m bash ./python/libcuopt/libcuopt/tests/test_cli.sh
+#timeout 10m bash ./python/libcuopt/libcuopt/tests/test_cli.sh
 
 # Run Python tests
-RAPIDS_DATASET_ROOT_DIR=./datasets timeout 30m python -m pytest --verbose --capture=no ./python/cuopt/cuopt/tests/
+#RAPIDS_DATASET_ROOT_DIR=./datasets timeout 30m python -m pytest --verbose --capture=no ./python/cuopt/cuopt/tests/
 
 # run cvxpy integration tests
-./ci/thirdparty-testing/run_cvxpy_tests.sh
+#./ci/thirdparty-testing/run_cvxpy_tests.sh
 
 # run jump tests
 ./ci/thirdparty-testing/run_jump_tests.sh
