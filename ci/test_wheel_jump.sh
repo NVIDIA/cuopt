bb35e902ff5ac6dbe0692cb218602894d3d33e36
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-09-19 15:01:08 -0500

    test

--- ci/test_wheel_jump.sh
+++ ci/test_wheel_jump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
-straints file for 'pip' and puts its location in an exported variable PIP_EXPORT,
+#straints file for 'pip' and puts its location in an exported variable PIP_EXPORT,
 # so those cons
 # sets up a contraints will affect all future 'pip install' calls
 source rapids-init-pip
