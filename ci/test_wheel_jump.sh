bb3cfdc9749a3ab12be90403920444b2252b5cd9
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-09-24 17:36:23 -0500

    remove unsued files

--- ci/test_wheel_jump.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-#straints file for 'pip' and puts its location in an exported variable PIP_EXPORT,
-# so those cons
-# sets up a contraints will affect all future 'pip install' calls
-source rapids-init-pip
-
-./ci/thirdparty-testing/run_jump_tests.sh
