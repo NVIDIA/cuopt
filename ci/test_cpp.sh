1e55bd6004c2dae28d2d33412a868f25aac13f74
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-09-30 10:55:28 -0500

    fix

--- ci/test_cpp.sh
+++ ci/test_cpp.sh
@@ -19,7 +19,7 @@ set -euo pipefail
 
 . /opt/conda/etc/profile.d/conda.sh
 
-CUOPT_VERSION="$(rapids-version)"
+#CUOPT_VERSION="$(rapids-version)"
 
 CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
 
--- ci/test_python.sh
+++ ci/test_python.sh
@@ -19,7 +19,7 @@ set -euo pipefail
 
 . /opt/conda/etc/profile.d/conda.sh
 
-CUOPT_VERSION="$(rapids-version)"
+#CUOPT_VERSION="$(rapids-version)"
 
 rapids-logger "Downloading artifacts from previous jobs"
 CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
