931441fdf3a803b5765aab248b96370d853a62ef
Alice Boucher <yboucher@nvidia.com>
2025-10-07 08:26:07 +0000

    fix typo

--- cpp/src/mip/feasibility_jump/fj_cpu.cu
+++ cpp/src/mip/feasibility_jump/fj_cpu.cu
@@ -338,9 +338,10 @@ static void apply_move(fj_cpu_climber_t<i_t, f_t>& fj_cpu,
   if (fj_cpu.h_incumbent_objective < fj_cpu.h_best_objective &&
       fj_cpu.violated_constraints.empty()) {
     // recompute the LHS values to cancel out accumulation errors, then check if feasibility remains
+    // 1
     recompute_lhs(fj_cpu);
 
-    if (!fj_cpu.violated_constraints.empty() && check_variable_feasibility<i_t, f_t>(fj_cpu.view)) {
+    if (fj_cpu.violated_constraints.empty() && check_variable_feasibility<i_t, f_t>(fj_cpu.view)) {
       cuopt_assert(fj_cpu.satisfied_constraints.size() == fj_cpu.view.pb.n_constraints, "");
       fj_cpu.h_best_objective =
         fj_cpu.h_incumbent_objective - fj_cpu.settings.parameters.breakthrough_move_epsilon;
