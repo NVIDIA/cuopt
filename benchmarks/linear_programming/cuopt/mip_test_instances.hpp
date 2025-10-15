91142044df3bc58d3f0bdcdab990331e6266bbe9
akifcorduk <akifcorduk@gmail.com>
2025-10-15 05:00:01 -0700

    fixed initial sol

--- benchmarks/linear_programming/cuopt/mip_test_instances.hpp
+++ benchmarks/linear_programming/cuopt/mip_test_instances.hpp
@@ -20,8 +20,8 @@
 std::vector<std::string> instances = {"polygonpack4-7_1.mps",
                                       "polygonpack4-7_2.mps",
                                       "polygonpack4-7_3.mps",
-                                      "neos-3355120-tarago_1.sol",
-                                      "neos-3355120-tarago_2.sol",
-                                      "neos-3355120-tarago_3.sol",
-                                      "liu_1.sol",
-                                      "liu_2.sol"};
+                                      "neos-3355120-tarago_1.mps",
+                                      "neos-3355120-tarago_2.mps",
+                                      "neos-3355120-tarago_3.mps",
+                                      "liu_1.mps",
+                                      "liu_2.mps"};
