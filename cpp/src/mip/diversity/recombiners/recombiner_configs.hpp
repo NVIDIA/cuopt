4a2c90fa2beb309b583d983f14a8bac01455702e
akifcorduk <akifcorduk@gmail.com>
2025-10-17 05:52:33 -0700

    longer recombiner times

--- cpp/src/mip/diversity/recombiners/recombiner_configs.hpp
+++ cpp/src/mip/diversity/recombiners/recombiner_configs.hpp
@@ -20,7 +20,7 @@
 namespace cuopt::linear_programming::detail {
 
 struct bp_recombiner_config_t {
-  static constexpr double bounds_prop_time_limit          = 1.;
+  static constexpr double bounds_prop_time_limit          = 2.;
   static constexpr double lp_after_bounds_prop_time_limit = 2.;
   // number of repair iterations even if it fails during the repair
   static constexpr size_t n_repair_iterations          = 10;
@@ -53,7 +53,7 @@ struct ls_recombiner_config_t {
   // FIXME: not implemented yet
   static constexpr bool use_fj_for_rounding            = false;
   static constexpr int n_points_to_search              = 20;
-  static constexpr double time_limit                   = 1.;
+  static constexpr double time_limit                   = 2.;
   static constexpr size_t initial_n_of_vars_from_other = 200;
   static constexpr size_t max_different_var_limit      = 10000;
   static constexpr size_t min_different_var_limit      = 20;
@@ -80,7 +80,7 @@ struct ls_recombiner_config_t {
 
 struct fp_recombiner_config_t {
   static constexpr double infeasibility_detection_time_limit = 0.05;
-  static constexpr double fp_time_limit                      = 1.;
+  static constexpr double fp_time_limit                      = 2.;
   static constexpr double alpha                              = 0.99;
   static constexpr double alpha_decrease_factor              = 0.9;
   static constexpr size_t initial_n_of_vars_from_other       = 200;
@@ -109,7 +109,7 @@ struct fp_recombiner_config_t {
 
 struct sub_mip_recombiner_config_t {
   static constexpr size_t max_continuous_vars                = 5000;
-  static constexpr double sub_mip_time_limit                 = 1.;
+  static constexpr double sub_mip_time_limit                 = 2.;
   static constexpr double infeasibility_detection_time_limit = 0.05;
   static constexpr size_t initial_n_of_vars_from_other       = 40;
   static constexpr size_t max_different_var_limit            = 500;
