0c8550b2e5cbb5e31dfa42eb9f0f998dad70093e
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-10-21 11:13:23 -0500

    disbale all barrier tests

--- python/cuopt_server/cuopt_server/tests/test_lp.py
+++ python/cuopt_server/cuopt_server/tests/test_lp.py
@@ -148,18 +148,20 @@ def test_sample_milp(
     )
 
 
+@pytest.mark.skip(
+    reason="Enable barrier solver options when issue "
+    "https://github.com/NVIDIA/cuopt/issues/519 is resolved",
+)
 @pytest.mark.parametrize(
     "folding, dualize, ordering, augmented, eliminate_dense, cudss_determ, "
     "dual_initial_point",
     [
         # Test automatic settings (default)
         (-1, -1, -1, -1, True, False, -1),
-        # Enable barrier solver options when this issue is resolved
-        # https://github.com/NVIDIA/cuopt/issues/519
         # Test folding off, no dualization, cuDSS default ordering, ADAT system
-        # (0, 0, 0, 0, True, False, 0),
+        (0, 0, 0, 0, True, False, 0),
         # Test folding on, force dualization, AMD ordering, augmented system
-        # (1, 1, 1, 1, True, True, 1),
+        (1, 1, 1, 1, True, True, 1),
         # Test mixed settings: automatic folding, no dualize, AMD, augmented
         (-1, 0, 1, 1, False, False, 0),
         # Test no folding, automatic dualize, cuDSS default, ADAT
