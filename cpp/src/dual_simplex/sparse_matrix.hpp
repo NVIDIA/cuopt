32ba29d51be67427f886eaba194d24860a75ba8e
Rajesh Gandham <rgandham@nvidia.com>
2025-10-30 14:12:17 -0700

    Debug info

--- cpp/src/dual_simplex/sparse_matrix.hpp
+++ cpp/src/dual_simplex/sparse_matrix.hpp
@@ -23,6 +23,7 @@
 #include <algorithm>
 #include <cassert>
 #include <cstdio>
+#include <string>
 #include <vector>
 
 namespace cuopt::linear_programming::dual_simplex {
@@ -57,6 +58,7 @@ class csc_matrix_t {
   // Adjust to i and x vectors for a new number of nonzeros
   void reallocate(i_t new_nz);
 
+  i_t nnz() const { return col_start[n]; }
   // Convert the CSC matrix to a CSR matrix
   i_t to_compressed_row(
     cuopt::linear_programming::dual_simplex::csr_matrix_t<i_t, f_t>& Arow) const;
@@ -103,7 +105,7 @@ class csc_matrix_t {
   void print_matrix(FILE* fid) const;
 
   // Ensures no repeated row indices within a column
-  i_t check_matrix() const;
+  i_t check_matrix(std::string matrix_name = "") const;
 
   // Writes the matrix to a file in Matrix Market format
   void write_matrix_market(FILE* fid) const;
@@ -147,7 +149,7 @@ class csr_matrix_t {
   i_t remove_rows(std::vector<i_t>& row_marker, csr_matrix_t<i_t, f_t>& Aout) const;
 
   // Ensures no repeated column indices within a row
-  void check_matrix() const;
+  void check_matrix(std::string matrix_name = "") const;
 
   i_t nz_max;                  // maximum number of nonzero entries
   i_t m;                       // number of rows
