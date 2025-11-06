1183a41da9928a309b09236ce945c53a2f4bb3bf
Kyle Edwards <kyedwards@nvidia.com>
2025-11-06 17:23:37 -0500

    Remove useless noqa statements

--- python/cuopt_self_hosted/cuopt_sh_client/cuopt_sh.py
+++ python/cuopt_self_hosted/cuopt_sh_client/cuopt_sh.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python3
 
-# noqa
 # SPDX-FileCopyrightText: Copyright (c) 2023-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
--- python/cuopt_server/cuopt_server/utils/routing/validation_fleet_data.py
+++ python/cuopt_server/cuopt_server/utils/routing/validation_fleet_data.py
@@ -1,4 +1,3 @@
-# noqa
 # SPDX-FileCopyrightText: Copyright (c) 2022-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
