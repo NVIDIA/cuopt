3317eaa928a7072e8314d28818eb3fe68baf024b
nicolas <nguidotti@nvidia.com>
2025-11-10 15:40:50 +0100

    fix missing initialization

--- cpp/src/dual_simplex/presolve.cpp
+++ cpp/src/dual_simplex/presolve.cpp
@@ -574,6 +574,7 @@ void convert_user_problem(const user_problem_t<i_t, f_t>& user_problem,
 
     // Empty var_types means that all variables are continuous
     bounds_strengthening_t<i_t, f_t> strengthening(problem, Arow, row_sense, {});
+    std::fill(strengthening.bounds_changed.begin(), strengthening.bounds_changed.end(), true);
     strengthening.bounds_strengthening(problem.lower, problem.upper, settings);
   }
 
