fd3ab737d0c920a96d3f87cc89663d85ccb31f3c
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-11-19 13:34:52 -0600

    disable tsp tests

--- python/cuopt/cuopt/tests/routing/test_tsp.py
+++ python/cuopt/cuopt/tests/routing/test_tsp.py
@@ -23,7 +23,8 @@ def data_(request):
     file_name = request.param
     return df, file_name
 
-
+# TO-DO: Remove this skip once the TSP Link is fixed and issue #609 is closed
+@pytest.mark.skip(reason="Skipping TSP tests")
 def test_tsp(data_):
     df, file_name = data_
     # read reference, if it doesn't exists skip the test
