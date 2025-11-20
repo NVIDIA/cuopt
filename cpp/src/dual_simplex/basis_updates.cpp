c8ecca2456171a01dfc4b391d5a82033d712cc6a
nicolas <nguidotti@nvidia.com>
2025-11-20 22:15:30 +0100

    removing debugging leftovers

--- cpp/src/dual_simplex/basis_updates.cpp
+++ cpp/src/dual_simplex/basis_updates.cpp
@@ -1193,7 +1193,7 @@ i_t basis_update_mpf_t<i_t, f_t>::scatter_into_workspace(const sparse_vector_t<i
 template <typename i_t, typename f_t>
 void basis_update_mpf_t<i_t, f_t>::grow_storage(i_t nz, i_t& S_start, i_t& S_nz)
 {
-  const i_t last_S_col = 2 * num_updates_;
+  const i_t last_S_col = num_updates_ * 2;
   assert(S_.n == last_S_col);
   const i_t new_last_S_col = last_S_col + 2;
   if (new_last_S_col >= S_.col_start.size()) {
@@ -1917,8 +1917,6 @@ i_t basis_update_mpf_t<i_t, f_t>::update(const sparse_vector_t<i_t, f_t>& utilde
   i_t S_start;
   i_t S_nz;
   grow_storage(nz + etilde.i.size(), S_start, S_nz);
-
-  if (nz + etilde.i.size() > S_.i.size()) {}
 
   S_.append_column(nz, xi_workspace_.data() + m, x_workspace_.data());
 
