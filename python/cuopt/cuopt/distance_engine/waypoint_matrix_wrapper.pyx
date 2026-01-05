0e668990daa9563b6e34de90925bfbd9ae600d55
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2026-01-05 10:30:41 -0600

    fix copyright

--- python/cuopt/cuopt/distance_engine/waypoint_matrix_wrapper.pyx
+++ python/cuopt/cuopt/distance_engine/waypoint_matrix_wrapper.pyx
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved. # noqa
+# SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved. # noqa
 # SPDX-License-Identifier: Apache-2.0
 
 # cython: profile=False
