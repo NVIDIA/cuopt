f2384f1b5b01b25d61f42cb48bf5bf85e819872f
Rajesh Gandham <rgandham@nvidia.com>
2026-01-13 11:56:06 -0800

    Directly check for num rows instead of row nz

--- cpp/src/dual_simplex/barrier.cu
+++ cpp/src/dual_simplex/barrier.cu
@@ -307,7 +307,7 @@ class iteration_data_t {
     settings.log.printf("Density estimator time      : %.2fs\n", column_density_time);
     if ((settings.augmented != 0) &&
         (n_dense_columns > 50 || n_dense_rows > 10 ||
-         max_row_nz == 0 /* handle case with no constraints */ ||
+         lp.A.m == 0 /* handle case with no constraints */ ||
          (max_row_nz > 5000 && estimated_nz_AAT > 1e10) || settings.augmented == 1)) {
       use_augmented   = true;
       n_dense_columns = 0;
