f9c7833efea95f69b69104eebbec37eb369eedfe
Christopher Maes <cmaes@nvidia.com>
2026-01-30 15:59:05 -0800

    More clean up

--- cpp/include/cuopt/linear_programming/mip/solver_settings.hpp
+++ cpp/include/cuopt/linear_programming/mip/solver_settings.hpp
@@ -81,7 +81,6 @@ class mip_solver_settings_t {
 
   f_t time_limit                = std::numeric_limits<f_t>::infinity();
   i_t node_limit                = std::numeric_limits<i_t>::max();
-  i_t reliability_branching     = -1;
   bool heuristics_only          = false;
   i_t num_cpu_threads           = -1;  // -1 means use default number of threads in branch and bound
   i_t max_cut_passes            = 0;   // number of cut passes to make
