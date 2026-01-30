10f33f0e3b330462f2cee6e9be2791d67ea82bac
James Lamb <jlamb@nvidia.com>
2026-01-30 13:40:48 -0600

    loosen CUDA 13 threshold

--- ci/validate_wheel.sh
+++ ci/validate_wheel.sh
@@ -26,7 +26,7 @@ if [[ "${package_dir}" == "python/libcuopt" ]]; then
         )
     else
         PYDISTCHECK_ARGS+=(
-            --max-allowed-size-compressed '420Mi'
+            --max-allowed-size-compressed '450Mi'
         )
     fi
 elif [[ "${package_dir}" != "python/cuopt" ]] && \
