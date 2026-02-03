726896b134ccf60f326e411005721de9d71057c1
Christopher Maes <cmaes@nvidia.com>
2026-02-03 11:56:11 -0800

    Add cuts unit test. Remove old unit test for dual simplex cuts

--- cpp/src/dual_simplex/branch_and_bound.cpp
+++ cpp/src/dual_simplex/branch_and_bound.cpp
@@ -2033,6 +2033,9 @@ mip_status_t branch_and_bound_t<i_t, f_t>::solve(mip_solution_t<i_t, f_t>& solut
       f_t rel_gap = user_relative_gap(original_lp_, upper_bound_.load(), root_objective_);
       f_t abs_gap = upper_bound_.load() - root_objective_;
       if (rel_gap < settings_.relative_mip_gap_tol || abs_gap < settings_.absolute_mip_gap_tol) {
+        if (settings_.solution_callback != nullptr) {
+          settings_.solution_callback(root_relax_soln_.x, root_objective_);
+        }
         set_final_solution(solution, root_objective_);
         return mip_status_t::OPTIMAL;
       }
