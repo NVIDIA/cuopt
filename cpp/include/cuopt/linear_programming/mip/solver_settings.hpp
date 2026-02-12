373ede6cd22ca7bd6509c1bc7c30264e17eb6272
akifcorduk <akifcorduk@gmail.com>
2026-02-12 06:00:08 -0800

    without clique cuts

--- cpp/include/cuopt/linear_programming/mip/solver_settings.hpp
+++ cpp/include/cuopt/linear_programming/mip/solver_settings.hpp
@@ -93,7 +93,7 @@ class mip_solver_settings_t {
   i_t mir_cuts                  = -1;
   i_t mixed_integer_gomory_cuts = -1;
   i_t knapsack_cuts             = -1;
-  i_t clique_cuts               = -1;
+  i_t clique_cuts               = 0;
   i_t strong_chvatal_gomory_cuts      = -1;
   i_t reduced_cost_strengthening      = -1;
   f_t cut_change_threshold            = 1e-3;
