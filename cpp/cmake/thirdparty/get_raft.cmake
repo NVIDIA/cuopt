23287ab141f9318e79a73ad0dade57a16f746b12
aamijar <aamijar230@gmail.com>
2026-02-13 17:24:04 +0000

    ci-test-raft

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -1,5 +1,5 @@
 # cmake-format: off
-# SPDX-FileCopyrightText: Copyright (c) 2021-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2021-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 # cmake-format: on
 
@@ -39,7 +39,7 @@ endfunction()
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION ${CUOPT_MIN_VERSION_raft}
-    FORK rapidsai
-    PINNED_TAG ${rapids-cmake-checkout-tag}
+    FORK aamijar
+    PINNED_TAG raft-deprecated-headers
     CLONE_ON_PIN ON
 )
