c8324c18028ed6617fee7b57f39d8261091b849c
Christopher Maes <cmaes@nvidia.com>
2026-02-17 14:59:39 -0800

    Handle case where some nodes should still be in the queue

--- cpp/src/branch_and_bound/branch_and_bound.cpp
+++ cpp/src/branch_and_bound/branch_and_bound.cpp
@@ -2484,9 +2484,11 @@ mip_status_t branch_and_bound_t<i_t, f_t>::solve(mip_solution_t<i_t, f_t>& solut
             settings_.log, start_node.value(), "cutoff", start_node.value()->lower_bound);
           search_tree_.update(start_node.value(), node_status_t::FATHOMED);
           continue;
+        } else {
+          break;
         }
       }
-      lower_bound = search_tree_.root.lower_bound;
+      lower_bound = node_queue_.best_first_queue_size() > 0 ? node_queue_.get_lower_bound() : search_tree_.root.lower_bound;
     } else {
       lower_bound = search_tree_.root.lower_bound;
     }
