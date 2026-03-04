ca1a0bbd7a740cf4b9fcf4baad6c59af5bcd004e
Ramakrishnap <42624703+rgsl888prabhu@users.noreply.github.com>
2026-03-04 14:38:34 +0530

    Update routing_changed.sh

--- ci/utils/routing_changed.sh
+++ ci/utils/routing_changed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Prints "true" if routing-related files changed vs origin/main, else "false".
