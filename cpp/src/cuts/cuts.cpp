395d5f0761b467d7a37918a4ad2dd4f29c9b027f
akifcorduk <akifcorduk@gmail.com>
2026-03-09 00:05:57 -0700

    higher work unit

--- cpp/src/cuts/cuts.cpp
+++ cpp/src/cuts/cuts.cpp
@@ -1170,7 +1170,7 @@ bool cut_generation_t<i_t, f_t>::generate_clique_cuts(
   // TODO this can be problem dependent
   const i_t max_calls         = 100000;
   f_t work_estimate           = 0.0;
-  const f_t max_work_estimate = 2e8;
+  const f_t max_work_estimate = 2e9;
 
   cuopt_assert(user_problem_.var_types.size() == static_cast<size_t>(num_vars),
                "User problem var_types size mismatch");
