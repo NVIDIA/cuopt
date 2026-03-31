2d585393b4136ae0015ba5001815903c7422236a
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2026-03-31 17:34:15 -0500

    bump libcuopt size by 5 mb

--- ci/validate_wheel.sh
+++ ci/validate_wheel.sh
@@ -22,7 +22,7 @@ PYDISTCHECK_ARGS=(
 if [[ "${package_dir}" == "python/libcuopt" ]]; then
     if [[ "${RAPIDS_CUDA_MAJOR}" == "12" ]]; then
         PYDISTCHECK_ARGS+=(
-            --max-allowed-size-compressed '645Mi'
+            --max-allowed-size-compressed '650Mi'
         )
     else
         PYDISTCHECK_ARGS+=(
