a7f79faab26a399958ffb6aad47ce511c7d1db8d
Nicolas Blin <nicolas.blin7@hotmail.fr>
2026-04-03 10:05:01 +0200

    style

--- cpp/include/cuopt/linear_programming/constants.h
+++ cpp/include/cuopt/linear_programming/constants.h
@@ -71,7 +71,7 @@
 #define CUOPT_MIP_CUT_CHANGE_THRESHOLD                "mip_cut_change_threshold"
 #define CUOPT_MIP_CUT_MIN_ORTHOGONALITY               "mip_cut_min_orthogonality"
 #define CUOPT_MIP_BATCH_PDLP_STRONG_BRANCHING         "mip_batch_pdlp_strong_branching"
-#define CUOPT_MIP_BATCH_PDLP_RELIABILITY_BRANCHING "mip_batch_pdlp_reliability_branching"
+#define CUOPT_MIP_BATCH_PDLP_RELIABILITY_BRANCHING    "mip_batch_pdlp_reliability_branching"
 #define CUOPT_MIP_STRONG_BRANCHING_SIMPLEX_ITER_LIMIT "mip_strong_branching_simplex_iter_limit"
 #define CUOPT_SOLUTION_FILE                           "solution_file"
 #define CUOPT_NUM_CPU_THREADS                         "num_cpu_threads"
