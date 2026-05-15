e57ea25a0c34c6f94c37a635c3660a3bc472047e
Christopher Maes <cmaes@nvidia.com>
2026-05-15 10:59:34 -0700

    Clean up PR

--- cpp/src/dual_simplex/presolve.cpp
+++ cpp/src/dual_simplex/presolve.cpp
@@ -1147,7 +1147,6 @@ i_t presolve(const lp_problem_t<i_t, f_t>& original,
         presolve_info.free_variable_indices.push_back(j);
       }
     }
-    settings.log.printf("Keeping %d free variables for QP augmented system\n", free_variables);
   } else if (settings.barrier_presolve && free_variables > 0) {
     // We have a variable x_j: with -inf < x_j < inf
     // we create new variables v and w with 0 <= v, w and x_j = v - w
