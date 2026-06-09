111a9f2fec4170eeac3d5df8f2564996d4b15561
Gil Forsyth <gforsyth@nvidia.com>
2026-06-09 14:56:13 -0400

    refactor: switch to `rapids-artifact-name` for consistent artifact naming

--- ci/build_wheel_cuopt_server.sh
+++ ci/build_wheel_cuopt_server.sh
@@ -12,3 +12,6 @@ package_dir="python/cuopt_server"
 ci/build_wheel.sh cuopt_server ${package_dir}
 cp "${package_dir}/dist"/* "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}/"
 ci/validate_wheel.sh "${package_dir}" "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
+
+RAPIDS_PACKAGE_NAME="$(rapids-artifact-name wheel_python cuopt-server cuopt --py "$RAPIDS_PY_VERSION" --cuda "$RAPIDS_CUDA_VERSION")"
+export RAPIDS_PACKAGE_NAME
--- ci/build_wheel_cuopt_sh_client.sh
+++ ci/build_wheel_cuopt_sh_client.sh
@@ -12,3 +12,6 @@ package_dir="python/cuopt_self_hosted"
 ci/build_wheel.sh cuopt_sh_client ${package_dir}
 cp "${package_dir}/dist"/* "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}/"
 ci/validate_wheel.sh "${package_dir}" "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
+
+RAPIDS_PACKAGE_NAME="$(rapids-artifact-name wheel_python cuopt-sh-client cuopt --py "$RAPIDS_PY_VERSION" --cuda "$RAPIDS_CUDA_VERSION")"
+export RAPIDS_PACKAGE_NAME
