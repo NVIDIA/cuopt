339b3c56de7e4a8cf983e5c86f412cdeae6b3c26
Nicolas L. Guidotti <nguidotti@nvidia.com>
2026-08-14 15:35:25 +0200

    shuffle the integer and fractional list to give an equal chance for all variables to be picked.
    Signed-off-by: Nicolas L. Guidotti <nguidotti@nvidia.com>

--- cpp/src/mip_heuristics/root_heuristics.hpp
+++ cpp/src/mip_heuristics/root_heuristics.hpp
@@ -58,8 +58,8 @@ struct cut_pass_heuristics_t {
     const simplex::simplex_solver_settings_t<i_t, f_t>& settings,
     f_t root_obj,
     const std::vector<simplex::variable_status_t>& root_vstatus,
-                            const std::vector<f_t>& sol,
-                            search_strategy_t type)
+    const std::vector<f_t>& sol,
+    search_strategy_t type)
   {
     submip_worker_ = std::make_unique<diving_worker_t<i_t, f_t>>(
       id, lp, Arow_, var_types_, settings, root_solution_, root_edge_norm_);
