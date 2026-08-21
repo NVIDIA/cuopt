914d4c2ba8533853af826929076c168cfedef148
Rajesh Gandham <rgandham@nvidia.com>
2026-08-21 16:03:53 -0700

    Move root relaxation solve archestration entirely to branch and bound code path

---
+++ cpp/src/branch_and_bound/concurrent_root_solver.hpp
@@ -0,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#pragma once
+
+#include <cuopt/mathematical_optimization/pdlp/solver_settings.hpp>
+
+#include <atomic>
+#include <vector>
+
+namespace cuopt::mathematical_optimization::mip {
+
+template <typename i_t, typename f_t>
+class problem_t;
+
+template <typename i_t, typename f_t>
+struct concurrent_root_solution_t {
+  bool usable{false};
+  bool optimal{false};
+  std::vector<f_t> primal;
+  std::vector<f_t> dual;
+  std::vector<f_t> reduced_cost;
+  f_t solver_objective{0};
+  f_t user_objective{0};
+  i_t iterations{0};
+  method_t method{method_t::Unset};
+};
+
+template <typename i_t, typename f_t>
+concurrent_root_solution_t<i_t, f_t> solve_concurrent_root_relaxation(
+  problem_t<i_t, f_t>* problem,
+  const pdlp_solver_settings_t<i_t, f_t>& settings,
+  f_t time_limit,
+  std::atomic<int>* concurrent_halt);
+
+}  // namespace cuopt::mathematical_optimization::mip
