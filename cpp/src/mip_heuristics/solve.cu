c10d8df0f127ebafa362966b622fd6d1372d33c1
yboucher <yboucher@nvidia.com>
2026-09-08 06:01:31 -0700

    fix tests

--- cpp/src/mip_heuristics/solve.cu
+++ cpp/src/mip_heuristics/solve.cu
@@ -896,6 +896,14 @@ mip_solution_t<i_t, f_t> solve_mip(optimization_problem_t<i_t, f_t>& op_problem,
       op_problem.get_handle_ptr()->get_stream()};
   }
 
+  // catch problem representation troubles early
+  try {
+    problem_checking_t<i_t, f_t>::check_problem_representation(op_problem);
+  } catch (const cuopt::logic_error& e) {
+    CUOPT_LOG_ERROR("Error in solve_mip: %s", e.what());
+    return mip_solution_t<i_t, f_t>{e, op_problem.get_handle_ptr()->get_stream()};
+  }
+
   // Run a very short burst of CPUFJ during the few dozen of milliseconds at solver init for very
   // small instances
   std::shared_ptr<mip::early_cpufj_t<i_t, f_t>> pre_solve_heuristics;
