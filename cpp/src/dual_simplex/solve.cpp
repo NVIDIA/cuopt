b74ab069a64914c6b58431e7056c45ff4e7d262a
yuwenchen95 <yuwchen@nvidia.com>
2026-09-18 01:35:08 -0700

    fix no variable issue after presolve
    Signed-off-by: yuwenchen95 <yuwchen@nvidia.com>

--- cpp/src/dual_simplex/solve.cpp
+++ cpp/src/dual_simplex/solve.cpp
@@ -508,8 +508,16 @@ lp_status_t solve_linear_program_with_barrier(
     cache->store_transform(std::move(xf));
   }
 
-  barrier::barrier_solver_t<i_t, f_t> barrier_solver(*solver_lp, presolve_info, barrier_settings);
-  lp_status_t barrier_status = barrier_solver.solve(start_time, barrier_solution, cache);
+  lp_status_t barrier_status;
+  if (solver_lp->num_cols == 0) {
+    // Presolve solved the problem, no need to run barrier
+    settings.log.printf("Presolve solved the problem, skipping barrier\n");
+    barrier_solution.user_objective = compute_user_objective(*solver_lp, static_cast<f_t>(0.0));
+    barrier_status                  = lp_status_t::OPTIMAL;
+  } else {
+    barrier::barrier_solver_t<i_t, f_t> barrier_solver(*solver_lp, presolve_info, barrier_settings);
+    barrier_status = barrier_solver.solve(start_time, barrier_solution, cache);
+  }
 
   if (cache != nullptr) {
     if (barrier_status == lp_status_t::OPTIMAL) {
