bd5c396263b0d14cd2727d6259a9fb29d7e76488
Trevor McKay <tmgithub1@gmail.com>
2026-09-21 12:32:06 -0400

    grpc harden test for initial solutions
    Add a logging line for MIP that can be checked. For LP,
    test that an initial solution results in "far less"
    iterations than without it (where far less is somewhat arbitrary)

--- cpp/src/mip_heuristics/solve.cu
+++ cpp/src/mip_heuristics/solve.cu
@@ -416,6 +416,11 @@ mip_solution_t<i_t, f_t> solve_mip_helper(
     problem_checking_t<i_t, f_t>::check_problem_representation(op_problem);
     problem_checking_t<i_t, f_t>::check_initial_solution_representation(op_problem, settings);
 
+    if (!settings.initial_solutions.empty()) {
+      CUOPT_LOG_INFO("Using %zu user-provided initial MIP solution(s)",
+                     settings.initial_solutions.size());
+    }
+
     CUOPT_LOG_INFO(
       "Solving a problem with %d constraints, %d variables (%d integers), and %d nonzeros",
       op_problem.get_n_constraints(),
