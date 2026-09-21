d8ed289db768b58c7ae2e45a2c8425e42f2bd065
yboucher <yboucher@nvidia.com>
2026-09-21 05:24:49 -0700

    Don't clear interrupted nodes when the time limit is hit in determinsitic mode

--- cpp/src/branch_and_bound/branch_and_bound.cpp
+++ cpp/src/branch_and_bound/branch_and_bound.cpp
@@ -4495,6 +4495,12 @@ void branch_and_bound_t<i_t, f_t>::run_deterministic_bfs_loop(
       worker.recompute_bounds_and_basis = !is_child;
 
       node_status_t status    = solve_node_deterministic(worker, node, search_tree);
+
+      if (status == node_status_t::PENDING) {
+        deterministic_scheduler_->wait_for_next_sync(worker.work_context);
+        continue;
+      }
+
       worker.last_solved_node = node;
 
       worker.current_node = nullptr;
