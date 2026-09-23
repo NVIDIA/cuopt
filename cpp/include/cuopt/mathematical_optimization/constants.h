ee91ab368b13217e0f00f16c16f075a0ca049504
Hugo Linsenmaier <hlinsenmaier@gmail.com>
2026-09-22 19:12:02 -0700

    Add NNZ cutoff for concurrent mode (#1932)
    Authors:
      - Hugo Linsenmaier (https://github.com/hlinsen)
    
    Approvers:
      - Chris Maes (https://github.com/chris-maes)
    
    URL: https://github.com/NVIDIA/cuopt/pull/1932

--- cpp/include/cuopt/mathematical_optimization/constants.h
+++ cpp/include/cuopt/mathematical_optimization/constants.h
@@ -36,6 +36,7 @@
 #define CUOPT_NODE_LIMIT                            "node_limit"
 #define CUOPT_PDLP_SOLVER_MODE                      "pdlp_solver_mode"
 #define CUOPT_METHOD                                "method"
+#define CUOPT_CONCURRENT_NNZ_CUTOFF                 "concurrent_nnz_cutoff"
 #define CUOPT_PER_CONSTRAINT_RESIDUAL               "per_constraint_residual"
 #define CUOPT_SAVE_BEST_PRIMAL_SO_FAR               "save_best_primal_so_far"
 #define CUOPT_FIRST_PRIMAL_FEASIBLE                 "first_primal_feasible"
